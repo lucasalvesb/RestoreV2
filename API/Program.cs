dbcc6ca7d499e971f0edf69df38a1e905a5ea400
Lucas Alves <lucasalves94@gmail.com>
2025-02-15 23:33:44 -0300

    cleaning program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -7,15 +7,6 @@ builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
